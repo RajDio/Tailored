ea68ff9072a0a3d1ff87f9babd910ba8e8128115
Raj Desai <rajdesai94@gmail.com>
2017-02-28 13:32:24 -0500

    Bare bones setup complete, webpack configured

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import Components
+// import StatefulComponent from './StatefulComponent';
+//
+// ReactDOM.render(StatefulComponent, document.getElementById('app'));
